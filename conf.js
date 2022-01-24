62c6b887aa71a94d28c7e6859587f4d680e5f456
waterwelly <68794849+waterwelly@users.noreply.github.com>
2022-01-24 14:47:09 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '116',
+  ver: '117',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -34,8 +34,8 @@ jsproxy_config({
         'b.jsproxy.workers.dev': 1,
       }
     },
-    'cfworker_2_': {
-      label: '更快',
+    '2': {
+      label: '更快[推荐]',
       lines: {
         'b.007.workers.dev': 1,
         'b.hehe.workers.dev': 1,
@@ -48,7 +48,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
+  node_default: '2',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
